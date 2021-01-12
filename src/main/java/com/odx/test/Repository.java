b85393b58f993af4f8bd8775a2decd8ad07f3e71
Chris You <28457425+hellochrisyou@users.noreply.github.com>
2021-01-12 16:15:19 -0500

    Comments added

--- src/main/java/com/odx/test/Repository.java
+++ src/main/java/com/odx/test/Repository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 // Extends CrudRepository for JPA implementation.  Can Add Repository annotation if desired.
+// The Java Persistence API (JPA) is a Java specification for accessing, persisting, and managing data between Java objects / classes and a relational database. 
 public interface Repository extends CrudRepository<DemoEntity, Long> {
 
 	  String findByName(String name);
