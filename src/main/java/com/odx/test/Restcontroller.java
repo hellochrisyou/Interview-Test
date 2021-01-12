da8eafa34194566f893378044c0b9dfb53046e46
Chris You <28457425+hellochrisyou@users.noreply.github.com>
2021-01-12 15:38:21 -0500

    Update

--- src/main/java/com/odx/test/Restcontroller.java
+++ src/main/java/com/odx/test/Restcontroller.java
@@ -15,34 +15,32 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Restcontroller {
-	@Autowired
-    private JdbcTemplate jdbcTemplate;
+	
 	@Autowired
 	private Repository repo;
 	@RequestMapping(method = RequestMethod.POST,
                     value = "/",
                     produces = MediaType.APPLICATION_JSON_VALUE)
     public void receiveString(@RequestBody String str) {
-	    int n = str.length(), maxLength = 1, start = 0;
-	    
+	    int end = 1, beginning = 0;
+	    // Two outer loops pick all sub strings starting from corner to corner
 		for (int i = 0; i < str.length(); i++) {
 	        for (int j = i; j < str.length(); j++) {
-	            int flag = 1;
-	 
-	            // Check palindrome
+	            boolean isPalindrome = true;
+	            // Inner loop check palindrome
 	            for (int k = 0; k < (j - i + 1) / 2; k++)
 	                if (str.charAt(i + k) != str.charAt(j - k))
-	                    flag = 0;
+	                    isPalindrome = false;
 	 
-	            // Palindrome
-	            if (flag!=0 && (j - i + 1) > maxLength) {
-	                start = i;
-	                maxLength = j - i + 1;
+	            // If palindrome, then record beginning and end indices
+	            if (isPalindrome == true && (j - i + 1) > end) {
+	            	beginning = i;
+	                end = j - i + 1;
 	            }
 	        }
 		}
 		StringBuilder sb = new StringBuilder();
-	    for (int i = start; i <= start + maxLength -1; ++i) {		        
+	    for (int i = beginning; i <= beginning + end -1; ++i) {		        
 	    	sb.append(str.charAt(i));
 	    }
 	    List<DemoEntity> thisRepo = (List<DemoEntity>) repo.findAll();
